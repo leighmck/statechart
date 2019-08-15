c9e08d3d82eb5d2640db274eb4670e02de7f9a63
Leigh McKenzie <maccarav0@gmail.com>
2019-08-15 23:10:34 +1000

    Fix display of guard function names

--- statechart/display.py
+++ statechart/display.py
@@ -254,7 +254,7 @@ class Display:
                 end=end,
                 div=' :' if transition.event or transition.action or transition.guard else '',
                 event=' {}'.format(transition.event.name) if transition.event else '',
-                guard=' [{}]'.format(transition.guard) if transition.guard else '',
+                guard=' [{}]'.format(transition.guard.__name__) if transition.guard else '',
                 action='{}'.format(' / ' + transition.action.__name__ if transition.action else '')
             )
         ]
